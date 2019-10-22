d1eddda15fb8a5f3dd42643e8e84f8d7e298f94e
Irony <892768447@qq.com>
2019-10-22 23:47:06 +0800

    调整结构

---
+++ QQBot/BotHandlers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Created on 2019年10月22日
+@author: Irony
+@site: https://pyqt5.com https://github.com/892768447
+@email: 892768447@qq.com
+@file: QQBot.BotHandlers
+@description: 
+"""
+
+__Author__ = 'Irony'
+__Copyright__ = 'Copyright (c) 2019'
+__Version__ = 1.0
+
+Handlers = []
